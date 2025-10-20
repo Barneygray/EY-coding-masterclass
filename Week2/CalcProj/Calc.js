3da78a910b59d4550df6b36af6eba4ad726b51af
Barney Gray <barney.gray@uk.ey.com>
2025-10-20 14:19:20 +0100

    first completion

--- Week2/CalcProj/Calc.js
+++ Week2/CalcProj/Calc.js
@@ -1,4 +1,4 @@
-const number = document.querySelector(".grey-buttons")
+const number = document.querySelectorAll(".number-button")
 
 const clearButton = document.querySelector(".C-button");
 const arrowButton = document.querySelector(".arrow-button");
@@ -8,50 +8,64 @@ const divButton = document.querySelector(".div-button");
 const xButton = document.querySelector(".x-button");
 const equalsButton = document.querySelector(".equals-button");
 
-const display = document.querySelector(".input-screen")
+const display = document.querySelector(".input-screen");
 
 let numStore = [];
 let numShow = [];
 let operations = [];
 
-display.innerText = numShow.join("");
-
-number.addEventListener("click", function() {
-    numStore.push(parseInt(number.innerText));
-    numShow.push(number.innerText);
+number.forEach(button => {
+    button.addEventListener("click", function() {
+        console.log(button.innerText);
+        numShow.push(button.innerText);
+        display.innerText = numShow.join("");
+        console.log(numStore)
+    });
 });
+
 
 clearButton.addEventListener("click", function() {
     numStore = [];
+    operations = [];
     numShow = [];
+    display.innerText = "0";
 });
 
 arrowButton.addEventListener("click", function() {
-    numStore.pop();
     numShow.pop();
+    display.innerText = numShow.join("");
+    display.innerText = numShow.length > 0 ? numShow.join("") : "0";
 });
 
 plusButton.addEventListener("click", function() {
+    numStore.push(Number(numShow.join("")));
     numShow = [];
     operations.push("+")
+    console.log(numStore);
 });
 
 minusButton.addEventListener("click", function() {
+    numStore.push(Number(numShow.join("")));
+    console.log(numStore[-1]);
     numShow = [];
     operations.push("-")
 });
 
 divButton.addEventListener("click", function() {
+    numStore.push(Number(numShow.join("")));
     numShow = [];
     operations.push("/")
 });
 
 xButton.addEventListener("click", function() {
+    numStore.push(Number(numShow.join("")));
     numShow = [];
-    operations.push("*")
+    operations.push("*");
 });
 
 equalsButton.addEventListener("click", function() {
+    numStore.push(Number(numShow.join("")))
+    console.log(numStore);
     let result = 0;
     for (let i = 0; i < operations.length; i++) {
         if (operations[i] === "+") {
@@ -64,10 +78,14 @@ equalsButton.addEventListener("click", function() {
             numStore[0] = numStore[0] / numStore[1];
             numStore.splice(1,1);
         } else if (operations[i] === "*") {
-            numStore[0] = numStore[0] - numStore[1];
+            numStore[0] = numStore[0] * numStore[1];
             numStore.splice(1,1);
         }
     }
     result = numStore[0];
-    numShow = [result];
+    numShow = [result.toString()];
+    numStore = [];
+    operations = [];
+    display.innerText = result;
+    console.log(result);
 })
