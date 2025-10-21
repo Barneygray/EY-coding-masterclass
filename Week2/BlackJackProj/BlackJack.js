d3edfd6cfb6e8f29e8fa5f26ca5a48b5ee5548af
Barney Gray <barney.gray@uk.ey.com>
2025-10-21 10:03:01 +0100

    user ace logic

--- Week2/BlackJackProj/BlackJack.js
+++ Week2/BlackJackProj/BlackJack.js
@@ -18,17 +18,21 @@ function shuffleDeck(deck) {
 }
 
 function calculatePlayerScore(hand) {
-    let score = 0;
+    let score1 = 0;
+    let score2 = 0;
     for (let i = 0; i < hand.length; i++) {
         if (hand[i][0] === 'A') {
-            score += 11;
-        } else if (['T', 'K', 'Q', 'J'].includes(hand[i][0])) {
-            score += 10;
+            score1 += 1;
+            score2 += 11;
+        } else if (['K', 'Q', 'J'].includes(hand[i][0])) {
+            score1 += 10;
+            score2 += 10;
         } else {
-            score += parseInt(hand[i].match(/\d+/g));
+            score1 += parseInt(hand[i].match(/\d+/g));
+            score2 += parseInt(hand[i].match(/\d+/g));
         }
     }
-    return score;
+    return [score1, score2];
 };
 
 function calculateDealerScore(hand) {
@@ -52,25 +56,42 @@ function play() {
     let userHand = [deck.pop(), deck.pop()];
     console.log("You have: " + userHand);
     let userScore = calculatePlayerScore(userHand);
-    console.log("Your Score Is: " + userScore);
+    if (userScore[0] === userScore[1] || userScore[1] > 21) {
+        console.log("Your Score Is: " + userScore[0]);
+    } else {
+        console.log("Your Score Is: " + userScore[0] + " or " + userScore[1]);
+    };
 
     let dealerHand = [deck.pop(), deck.pop()];
     console.log("Dealer shows: " + dealerHand[0]);
     let dealerScore = calculateDealerScore(dealerHand);
 
-    while (userScore < 21) {
+    while (userScore[0] < 21) {
         let response = globalThis.prompt("Hit or stand? (h/s): ");
         if (response && response.toLowerCase() === 'h') {
             userHand.push(deck.pop());
             console.log("You have: " + userHand);
             userScore = calculatePlayerScore(userHand);
-            console.log("Your Score Is: " + userScore);
+            if (userScore[0] === userScore[1] || userScore[1] > 21) {
+                console.log("Your Score Is: " + userScore[0]);
+            } else if (userScore[0] > 21) {
+                console.log("Your Score Is: " + userScore[0]);
+            } else if (userScore[1] === 21) {
+                console.log("your Score Is: " + userScore[1]);
+            } else{
+                console.log("Your Score Is: " + userScore[0] + " or " + userScore[1]);
+            };
 
-            if (userScore > 21) {
+            if (userScore[0] > 21) {
                 console.log("Bust! Dealer wins.");
                 return;
             }; 
         } else {
+            if (userScore[1] <= 21) {
+                userScore = userScore[1];
+            } else {
+                userScore = userScore[0];
+            };
             console.log("Dealer flips: " + dealerHand);
             console.log("Dealer's Score: " + dealerScore)
             break;
